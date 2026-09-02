ddf05fc8966ac3cbc289d33895f9ccb8db28830d
Kanishk Goyal <goyalk@uoguelph.ca>
2026-09-02 14:46:34 -0400

    neon setup

--- src/config/db.js
+++ src/config/db.js
@@ -6,13 +6,9 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: false,
+  connectionString: process.env.DB_URL
 });
+
 
 pool.on("connect", () => {
   console.log("Connected to PostgreSQL database");
